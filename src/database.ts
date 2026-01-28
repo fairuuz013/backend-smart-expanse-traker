7be4ab60ff5b442de41107c98c64dc098dfe5482
fairuuz <justcall1313@gmail.com>
2026-01-28 14:31:52 +0700

    add database deploy with neo

--- src/database.ts
+++ src/database.ts
@@ -1,33 +1,31 @@
 // src/database.ts
-import { PrismaClient } from "./generated"; // import dari folder generated
+import { PrismaClient } from "./generated";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import config from "./utils/env";
 
-// 1. Definisikan tipe global agar TypeScript tidak komplain
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-// 2. Fungsi inisialisasi Prisma dengan Adapter
 const prismaClientSingleton = () => {
+    // Neon menggunakan SSL, pastikan pool-nya menghandle ini
     const pool = new Pool({ 
         connectionString: config.DATABASE_URL 
     });
     
     const adapter = new PrismaPg(pool);
     
+    // Di Prisma 7.2, saat pakai adapter, 
+    // kita tidak perlu lagi mengoper datasourceUrl di sini
     return new PrismaClient({ 
         adapter,
-        // Tambahkan log biar enak debugging
         log: ['query', 'info', 'warn', 'error'], 
     });
 };
 
-// 3. Logic Singleton (Cek Global dulu)
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-export default prisma;
-
-// 4. Simpan ke global jika di mode development
-if (process.env.NODE_ENV !== "production") {
+if (config.NODE_ENV !== "production") {
     globalForPrisma.prisma = prisma;
 }
+
+export default prisma;
